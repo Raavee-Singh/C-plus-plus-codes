8e221eb10ca5118c367eb4a28c1ad3665b7ed971
Raavee Singh <gdg1465@gmail.com>
2025-12-26 13:30:44 +0530

    second commit

---
+++ arearectangle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a=10;
+    int b=5;
+    int area=a*b;
+    cout<<"Area of Rectangle is "<<area <<endl;
+    return 0;
+}
---
+++ displayvar.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age=18;
+    cout<< "My age is " <<age<< " years"<<endl; 
+    return 0;
+}
